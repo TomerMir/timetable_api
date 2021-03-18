de3bf54d82cf5508657d5b28fb576f022744a702
TomerMir <mtomer@gmail.com>
2021-03-18 19:11:46 +0200

    first commit

---
+++ api.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, jsonify, request
+from mysql_connect import fetch_from_database
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "mega-super-secret-key"  #IDC if you are seeing thins...
+jwt = JWTManager(app)
+
+@app.route("/login", methods=["POST"])
+def login():  
+
+    username = request.json.get("username")
+    password = request.json.get("password")
+    
+    logging.debug("New login request from username", username)
+
+    db_result = fetch_from_database("SELECT * FROM users WHERE username=\'" + username + "\' AND password_hash=\'" + password +'\';')
+    print(db_result)
+    if len(db_result) == 0:
+        return jsonify(status=False)
+
+    access_token = create_access_token(identity=username)
+    return jsonify(status=True, token=access_token)
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    logging.debug("New register request for username", username)
+
+    commit_to_database("INSERT INTO users (username, password) VALUES (%s, %s)", ("'"+username+"'", "'"+password+"'"))
+    return jsonify(status=True)
+    
+app.run()
---
+++ mysql_connect.py
@@ -0,0 +1,45 @@
+import mysql.connector
+import logging
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="*ErF9BvlQ-t08st:<hg,*M94-i3hGQ",
+        database="timetable_database"
+    )
+    logging.info("Connected to database")
+    cursor = mydb.cursor()
+    logging.debug("Got cursor")
+
+except:
+    logging.critical("Failed to connect to the database")
+    exit()
+
+def fetch_from_database(query : str) -> tuple:
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        logging.debug("Succusfuly fetched data from the database")
+        return result
+    except Exception as ex:
+        logging.error("Failed to fetch the database "+str(ex))
+        return  None
+
+def commit_to_database(query : str, values : tuple):
+    try:
+        cursor.execute(query, values)
+        mydb.commit()
+        logging.debug("Succusfuly commited to the database")
+
+    except Exception as ex:
+        logging.error("Failed to commit to the database "+str(ex))
+
+def commit_to_database(query : str):
+    try:
+        cursor.execute(query)
+        mydb.commit()
+        logging.debug("Succusfuly commited to the database")
+
+    except Exception as ex:
+        logging.error("Failed to commit to the database "+str(ex))
