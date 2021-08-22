c68fbcf5ec3c98435bb925e0fee8973ced4f8a65
TomerMir <mtomer@gmail.com>
2021-08-22 12:58:57 +0300

    Added a bit documentation and added debug arg

--- api.py
+++ api.py
@@ -4,21 +4,30 @@ from mysql_connect import *
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager, verify_jwt_in_request
 from flask_cors import CORS
 import logging
+from waitress import serve
+import argparse
 
-logging.basicConfig(level=logging.DEBUG)
+#Setup flask app and CORS (Cross-origin resource sharing)
 app = Flask(__name__)
 CORS(app)
 
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "g8@)#Eh?AufQv#Z@#(76*gsd,s`,z"  #IDC if you are seeing thins...
+# Setup the jwt
+app.config["JWT_SECRET_KEY"] = "g8@)#Eh?AufQv#Z@#(76*gsd,s`,z"
 jwt = JWTManager(app)
 
+#Setup the servers loggers
+werkzeugLogger = logging.getLogger('werkzeug')
+werkzeugLogger.setLevel(logging.ERROR)
+waitressLogger = logging.getLogger('waitress')
+waitressLogger.setLevel(logging.INFO)
+
+#Login
 @app.route("/api/login", methods=["POST"])
 def login():  
     username = request.json.get("username")
     password = request.json.get("password")
     
-    app.logger.debug("New login request from username "+username)
+    app.logger.info("New login request from "+username)
 
     db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s AND password_hash=%s;", (username, password))
     if len(db_result) == 0:
@@ -27,12 +36,14 @@ def login():
     access_token = create_access_token(identity=username, additional_claims={"admin" : admin}) 
     return jsonify(status=True, exp=15, token=access_token)
 
+#Register new user
 @app.route("/api/register", methods=["POST"])
 def register():
     username = request.json.get("username")
     password = request.json.get("password")
 
-    logging.debug("New register request for username", username) 
+    app.logger.info("New register request for "+username) 
+    
     db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s;", (username,))
     if db_result != None and len(db_result) > 0: 
         return jsonify(status=False)
@@ -45,21 +56,26 @@ def register():
         '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
         '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
         '0', '0', '0', '0', '0', '0', '0', '0', '0'))
+
     return jsonify(status=True)
 
+#JWT invalid route
 @jwt.invalid_token_loader
 def invalid_token_callback(callback):
     return jsonify(status=False, err="Invalid token")
 
+#JWT expired route
 @jwt.expired_token_loader
 def expired_token_callback(jwt_header, jwt_payload):
     return jsonify(status=False, err="Token expired")
 
+#Get the timetable
 @app.route("/api/gettable", methods=["GET"])
 @jwt_required()
 def get_table():
     try:
         username = get_jwt_identity()
+        app.logger.info("Get table request from "+ username) 
         db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s;", (username,))
         db_result = list(db_result[0])[3:]
         del db_result[-1]
@@ -67,11 +83,13 @@ def get_table():
     except Exception:
         return jsonify(status=False, err="Server error")
 
+#Change the timetable
 @app.route("/api/changetable", methods=["POST"])
 @jwt_required()
 def edit_table():
     try:
         username = get_jwt_identity()
+        app.logger.info("Change table request from "+ username) 
         new_data = request.json.get("data")
         query = "UPDATE users SET "
         for i in range(1, 7):
@@ -90,15 +108,18 @@ def edit_table():
     except Exception:
         return jsonify(status=False)
 
+#Verify if user is admin
 def verify_admin(username):
     db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s;", (username,))
     return db_result[0][-1] == 1
     
+#Get the users list
 @app.route("/api/getusers", methods=["GET"])
 @jwt_required()
 def get_users():
     try:
         username = get_jwt_identity()
+        app.logger.info("Get users request from "+ username) 
         if not verify_admin(username):
             return jsonify(status=False, err="You are not an admin")
 
@@ -108,7 +129,7 @@ def get_users():
     except Exception:
         return jsonify(status=False, err="Server error")
 
-
+#Change role for specific user
 @app.route("/api/changeadmin", methods=["POST"])
 @jwt_required()
 def change_admin():
@@ -117,6 +138,7 @@ def change_admin():
         if not verify_admin(username):
             return jsonify(status=False, err="You are not an admin")
         user_to_change = request.json.get("username")
+        app.logger.info("%s changed %s's role" % (username, user_to_change)) 
         if user_to_change == username:
             return jsonify(status=False, err="You can't change your own role")
         db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s;", (user_to_change,))
@@ -133,6 +155,8 @@ def change_admin():
     except Exception:
         return jsonify(status=False, err="Server error")
 
+
+#Delete user
 @app.route("/api/deleteuser", methods=["POST"])
 @jwt_required()
 def delete_user():
@@ -141,6 +165,7 @@ def delete_user():
         if not verify_admin(username):
             return jsonify(status=False, err="You are not an admin")
         user_to_change = request.json.get("username")
+        app.logger.info("%s deleted %s's account" % (username, user_to_change)) 
         if user_to_change == username:
             return jsonify(status=False, err="You can't delete your own user")
         db_result = fetch_from_database_values("SELECT * FROM users WHERE username=%s;", (user_to_change,))
@@ -152,5 +177,13 @@ def delete_user():
 
     except Exception:
         return jsonify(status=False, err="Server error")
+
+        
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port=5000, debug=True)
+    parser = argparse.ArgumentParser("Timetable api server")
+    parser.add_argument("debug", help="""1 for debug mode. 0 for production mode""",nargs='?', const=1, type=int, default=0)
+    args = parser.parse_args()
+    if args.debug == 1:
+        app.run(host= '0.0.0.0', port=5000, debug=True)
+    else:
+        serve(app, host='0.0.0.0', port=5000)
